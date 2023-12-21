e811acd561544617d3a77625795f3039b92d01d0
Jomerjom <ekenesjoachim@gmail.com>
2023-12-21 12:12:14 +0100

    documentation change

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import { createPropertyCard } from "./JS/createPropertyCard.js";
 import { createProjectList } from "./JS/createProjectList.js";
 
+// Build the website by running the createProjectList and createPropertyCard functions
 createProjectList();
 createPropertyCard(0);
