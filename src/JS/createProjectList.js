c4cf995c0539df5393d71c188e1b9804dc07f856
Jomerjom <ekenesjoachim@gmail.com>
2023-12-21 12:39:51 +0100

    added sitebar title and adjusted text size on buttons

--- src/JS/createProjectList.js
+++ src/JS/createProjectList.js
@@ -43,8 +43,8 @@ export const createProjectList = () => {
             button.id = `project${property.id}${property.navn}`;
             button.className = "flex items-center flex-row justify-start text-left bg-[#FFFFFF] border-[#DBDCDB] border-2 rounded-sm p-2 m-2 hover:bg-[#3C3E3E] hover:text-[#F5F7EF] min-w-full active:scale-95 transition duration-150 ease-in-out";
             button.innerHTML = `
-                <p class="text-sm mx-1">${property.id}.</p>
-                <p class="text-sm">${property.navn}</p>
+                <p class="text-base mx-1">${property.id}.</p>
+                <p class="text-base">${property.navn}</p>
             `;
 
             // Add a click event listener to the button
