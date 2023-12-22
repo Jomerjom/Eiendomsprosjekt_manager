ec0e1d5e9e7f895d84c06634ca29952f9d824b1a
tobias10kodehode <tobias10.kodehode@gmail.com>
2023-12-22 12:46:09 +0100

    Added converter for datestamping.

--- src/JS/timestampConverter.js
+++ src/JS/timestampConverter.js
@@ -1,22 +1,30 @@
-const jsonFilePath = 'src/assets/data/sampledata.json';
 
-fetch(jsonFilePath)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+let something = "2018-02-01"
+something = something.split("-")
+console.log(something)
+month = something[1]
+console.log(month)
+
+year = something[0]
+console.log(year)
+
+
+function konverterTilKvartal(year, måned) {
+
+    // Beregn kvartalet
+    let kvartal;
+    if (måned >= 1 && måned <= 3) {
+        kvartal = "Q1";
+    } else if (måned >= 4 && måned <= 6) {
+        kvartal = "Q2";
+    } else if (måned >= 7 && måned <= 9) {
+        kvartal = "Q3";
+    } else {
+        kvartal = "Q4";
     }
-    
-    return response.json();
-})
-  .then(data => {
-      console.log(data);
-      
-    // You can access individual properties
-    // console.log('Name:', data.name);
-    // console.log('Age:', data.age);
-    // console.log('City:', data.city);
-})
 
-.catch(error => {
-  console.error('Error during fetch:', error);
-});
+    // Returner resultatet
+    return kvartal + " " + year;
+}
+
+console.log(konverterTilKvartal(year, month))
--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import { createPropertyCard } from "./JS/createPropertyCard.js";
 import { createProjectList } from "./JS/createProjectList.js";
-import { timestampConverter } from "./JS/timestampConverter.js";
 // Build the website by running the createProjectList and createPropertyCard functions
 createProjectList();
 createPropertyCard(0);
